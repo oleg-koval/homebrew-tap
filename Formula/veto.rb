db79189d290a790002d62c7434ca229fc277267e
Oleg Koval <kvl.olg@gmail.com>
2026-08-29 14:43:20 +0200

    fix: make veto formula installable on macOS

--- Formula/veto.rb
+++ Formula/veto.rb
@@ -12,7 +12,7 @@ class Veto < Formula
       sha256 "12c791d330e1fad6ed1f75414332be1e0150a625d76f5035dea42a6a4e551967"
 
       define_method(:install) do
-        bin.install "veto_0.1.0_darwin_amd64/veto" => "veto"
+        bin.install "veto"
       end
     end
 
@@ -21,7 +21,7 @@ class Veto < Formula
       sha256 "e7470730db8b526aa3f9cd46c42bfcf490d858b294802152d4c893dfa878efc4"
 
       define_method(:install) do
-        bin.install "veto_0.1.0_darwin_arm64/veto" => "veto"
+        bin.install "veto"
       end
     end
   end
@@ -32,7 +32,7 @@ class Veto < Formula
       sha256 "1abe1cd10205c2f87fb20a62b696d701fb3aea667884b8d33bd9109866da80f5"
 
       define_method(:install) do
-        bin.install "veto_0.1.0_linux_amd64/veto" => "veto"
+        bin.install "veto"
       end
     end
 
@@ -41,9 +41,18 @@ class Veto < Formula
       sha256 "fc15414a9cbd659571c9894d49a422c17d4059ad97443cea3e59f30d385defe8"
 
       define_method(:install) do
-        bin.install "veto_0.1.0_linux_arm64/veto" => "veto"
+        bin.install "veto"
       end
     end
+  end
+
+  def post_install
+    return unless OS.mac?
+
+    executable = bin/"veto"
+    executable.chmod 0755
+    system "/usr/bin/xattr", "-d", "com.apple.quarantine", executable
+    executable.chmod 0555
   end
 
   test do
